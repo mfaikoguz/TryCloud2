a375f20ed1ef2ecc8931e52f3db1d0a88bef3bb8
Your Name <mfaikoguz@gmail.com>
2020-08-16 16:03:05 -0500

    loginstepDef

--- src/test/java/com/TryCloud/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/TryCloud/step_definitions/LoginStepDefinitions.java
@@ -1,28 +1,33 @@
 package com.TryCloud.step_definitions;
 
+import com.TryCloud.pages.LoginPage;
+import com.TryCloud.utilities.ConfigurationReader;
+import com.TryCloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
     @Given("User is on the login page")
     public void user_is_on_the_login_page() {
-
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get(ConfigurationReader.getProperty("tryCloud_url"));
     }
 
     @Then("User puts the username")
     public void user_puts_the_username() {
-
+        loginPage.usernameInput.sendKeys(ConfigurationReader.getProperty("username"));
     }
 
     @Then("User puts the password")
     public void user_puts_the_password() {
-
+        loginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("password"));
     }
 
     @Then("User clicks login button")
     public void user_clicks_login_button() {
-
+        loginPage.loginButton.click();
     }
 
 
