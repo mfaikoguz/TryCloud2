51c825f9c9286aee7cc227658d57a1025f4f7c8a
Your Name <mfaikoguz@gmail.com>
2020-08-16 15:35:52 -0500

    snippets

---
+++ src/test/java/com/TryCloud/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,29 @@
+package com.TryCloud.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class LoginStepDefinitions {
+
+    @Given("User is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+
+    @Then("User puts the username")
+    public void user_puts_the_username() {
+
+    }
+
+    @Then("User puts the password")
+    public void user_puts_the_password() {
+
+    }
+
+    @Then("User clicks login button")
+    public void user_clicks_login_button() {
+
+    }
+
+
+}
