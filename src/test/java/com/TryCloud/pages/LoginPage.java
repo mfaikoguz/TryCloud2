ca24f898d4125ee975cedf62c54b52e2d42cc838
Your Name <mfaikoguz@gmail.com>
2020-08-16 15:53:51 -0500

    login page

---
+++ src/test/java/com/TryCloud/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package com.TryCloud.pages;
+
+import com.TryCloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "user")
+    public WebElement usernameInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(id = "submit")
+    public WebElement loginButton;
+
+    @FindBy(id = "lost-password")
+    public WebElement forgotPassword;
+
+
+}
